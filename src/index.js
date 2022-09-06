ab55e69395fbb9771cdd1baf7a68e404ec866553
Finz-i <finzigerald963@gmail.com>
2022-09-06 15:48:40 +0300

    Try to add rotate animation

--- src/index.js
+++ src/index.js
@@ -8,23 +8,48 @@ document.addEventListener('DOMContentLoaded', () =>{
     audioNight.src = './sound/Night.wav'; //Sound for the day theme
 
     let themeBtn = document.getElementById('theme');
-
+    
     let themeIcon = document.getElementById('theme-icon');
 
-    theme.addEventListener('click', () =>{
+    let dayKeyframes = new KeyframeEffect(
+        themeIcon,
+        [
+            {rotate: '360deg'},
+            {rotate: '180deg'}
+        ],
+        {duration: 150, fill: "forwards"}
+    )
 
-        console.log(document.body.style.backgroundColor)
+    let nightKeyframes = new KeyframeEffect(
+        themeIcon,
+        [
+            {rotate: '180deg'},
+            {rotate: '360deg' }
+        ],
+        { duration: 150, fill: "forwards" }
+    )
+
+
+    let dayAnimate = new Animation(dayKeyframes, document.timeline);//First rotate
+    let nightAnimate = new Animation(nightKeyframes, document.timeline);//Second rotate
+
+    
+    theme.addEventListener('click', () =>{
         
         if (document.body.style.backgroundColor == 'rgb(52, 52, 52)'){
             audioDay.play();
+            dayAnimate.play();
             document.body.style.backgroundColor = 'rgb(242, 242, 242)';
             themeBtn.style.border = 'none';
-            themeBtn.style.backgroundColor = ''
-            themeIcon.style.transform = "rotate(180deg)";
+            /* themeIcon.style.transform = "rotate(180deg)"; */
+
+            
         } else if (document.body.style.backgroundColor == 'rgb(242, 242, 242)'){
             audioNight.play();
-            document.body.style.backgroundColor = 'rgb(52, 52, 52)'
-            themeIcon.style.transform = "rotate(360deg)";
+            nightAnimate.play()
+            document.body.style.backgroundColor = 'rgb(52, 52, 52)';
+            themeBtn.style.border = '1px';
+            /* themeIcon.style.transform = "rotate(360deg)"; */
         }
     })
 })
