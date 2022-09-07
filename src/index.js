5a8837c12673ea0a62e529a4ff764c54b6965f7f
Finz-i <finzigerald963@gmail.com>
2022-09-07 12:17:59 +0300

    Adding buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     document.body.style.backgroundColor = '#343434';
+
+    const player = document.querySelector('.player');//Player body
+    
 
     let audioDay = new Audio();
     audioDay.src = './sound/Day.wav'; //Sound for the night theme
@@ -8,48 +11,38 @@ document.addEventListener('DOMContentLoaded', () =>{
     audioNight.src = './sound/Night.wav'; //Sound for the day theme
 
     let themeBtn = document.getElementById('theme');
-    
+
     let themeIcon = document.getElementById('theme-icon');
 
-    let dayKeyframes = new KeyframeEffect(
+    let daynightKeyframes = new KeyframeEffect(
         themeIcon,
         [
-            {rotate: '360deg'},
-            {rotate: '180deg'}
+            { rotate: '360deg' }
         ],
-        {duration: 150, fill: "forwards"}
+        { duration: 180, fill: "forwards" }
     )
 
-    let nightKeyframes = new KeyframeEffect(
-        themeIcon,
-        [
-            {rotate: '180deg'},
-            {rotate: '360deg' }
-        ],
-        { duration: 150, fill: "forwards" }
-    )
-
-
-    let dayAnimate = new Animation(dayKeyframes, document.timeline);//First rotate
-    let nightAnimate = new Animation(nightKeyframes, document.timeline);//Second rotate
-
+    let daynightAnimate = new Animation(daynightKeyframes, document.timeline);
     
-    theme.addEventListener('click', () =>{
-        
-        if (document.body.style.backgroundColor == 'rgb(52, 52, 52)'){
+    let count = 1;
+    theme.addEventListener('click', () => {
+        count += 1;
+
+        if (count % 2 == 0) {
             audioDay.play();
-            dayAnimate.play();
+            daynightAnimate.play();
             document.body.style.backgroundColor = 'rgb(242, 242, 242)';
             themeBtn.style.border = 'none';
             /* themeIcon.style.transform = "rotate(180deg)"; */
 
-            
-        } else if (document.body.style.backgroundColor == 'rgb(242, 242, 242)'){
+        } else if (count % 2 != 0) {
             audioNight.play();
-            nightAnimate.play()
+            daynightAnimate.play()
             document.body.style.backgroundColor = 'rgb(52, 52, 52)';
-            themeBtn.style.border = '1px';
+            themeBtn.style.border = '2px';
             /* themeIcon.style.transform = "rotate(360deg)"; */
         }
-    })
+    }) 
+
+
 })
